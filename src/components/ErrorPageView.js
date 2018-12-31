8e6e67febfd0c5090084a8778244ac926c8a79c0
Dixit Keshavbhai Patel <dixitk13@gmail.com>
2018-12-31 10:57:51 -0800

    adding a simple 404 page

---
+++ src/components/ErrorPageView.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const ErrorPageView = ({}) => <div className="">404</div>;
+
+export default ErrorPageView;
---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import ErrorPageView from "../components/ErrorPageView";
+
+class ErrorPage extends Component {
+  render() {
+    return <ErrorPageView />;
+  }
+}
+
+export default ErrorPage;

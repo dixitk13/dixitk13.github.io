c1cfa8346aa30c993cc98d30f16860af0de296b1
Dixit Keshavbhai Patel <dixitk13@gmail.com>
2020-05-31 10:34:16 -0700

    Updates

--- svg/emojis.ts
+++ svg/emojis.ts
@@ -2,10 +2,12 @@ const chat = "💬";
 const communication = "📱";
 const process = "⚙️";
 const back = "🔙";
+const top = "🔝";
 
 export const Emojis = {
+  top,
+  back,
   chat,
   communication,
-  back,
   process,
 };

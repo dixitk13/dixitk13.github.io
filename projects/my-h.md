f1e8edfa4b3960c4a529c7508c01a8524e795300
Dixit Keshavbhai Patel <dixitk13@gmail.com>
2021-02-28 22:44:31 -0800

    Updates

---
+++ projects/my-h.md
@@ -0,0 +1,20 @@
+---
+title: "People List"
+date: "2020-01-01"
+link: "http://people-list.herokuapp.com/"
+type: "project"
+tags:
+  - react
+  - typescript
+  - react-hooks
+images:
+  - react
+  - typescript
+  - heroku
+---
+
+PeopleList using Typescript and React.
+
+- Build a custom Accordian for re-usability purposes.
+- Uses custom React hooks and Typescript for educational purposes.
+- Deployed on herokuapp.
---
+++ simple-icons/index.js
@@ -0,0 +1,82 @@
+import apollographql from "./apollographql.svg";
+import gatsby from "./gatsby.svg";
+import java from "./java.svg";
+import mysql from "./mysql.svg";
+import redis from "./redis.svg";
+import twitter from "./twitter.svg";
+import babel from "./babel.svg";
+import github from "./github.svg";
+import javascript from "./javascript.svg";
+import node from "./node-dot-js.svg";
+import redux from "./redux.svg";
+import typescript from "./typescript.svg";
+import cisco from "./cisco.svg";
+import graphql from "./graphql.svg";
+import kibana from "./kibana.svg";
+import python from "./python.svg";
+import rss from "./rss.svg";
+import webpack from "./webpack.svg";
+import d3 from "./d3-dot-js.svg";
+import heroku from "./heroku.svg";
+import linux from "./linux.svg";
+import quora from "./quora.svg";
+import strava from "./strava.svg";
+import elasticsearch from "./elasticsearch.svg";
+import logstash from "./logstash.svg";
+import react from "./react.svg";
+import styledcomponents from "./styled-components.svg";
+
+// project images
+import blueprintjsIcon from "../svg/blueprintjs.svg";
+import craIcon from "../svg/cra.png";
+import graphIcon from "../svg/graph.png";
+import pwaIcon from "../svg/pwa.svg";
+import genesysIcon from "../svg/genesys.png";
+import infosysIcon from "../svg/infosys.svg";
+import northeasternIcon from "../svg/northeastern.svg";
+import hiberanteIcon from "../svg/hibernate.svg";
+import reflectionsIcon from "../svg/reflections.svg";
+import computerscienceIcon from "../svg/computerscience.svg";
+import hatIcon from "../svg/hat.png";
+
+export const Icons = {
+  heroku,
+  typescript,
+  node,
+  redis,
+  gatsby,
+  react,
+  graphql,
+  webpack,
+  babel,
+  strava,
+  twitter,
+  quora,
+  github,
+  cisco,
+  javascript,
+  sql: mysql,
+  python: python,
+  redux,
+  cra: craIcon,
+  d3js: d3,
+  java: java,
+  graph: graphIcon,
+  pwa: pwaIcon,
+  linux,
+  elasticsearch,
+  logstash,
+  kibana,
+  rss,
+  styledcomponents,
+  "styled-components": styledcomponents,
+  blueprintjs: blueprintjsIcon,
+  genesys: genesysIcon,
+  northeastern: northeasternIcon,
+  infosys: infosysIcon,
+  hibernate: hiberanteIcon,
+  reflections: reflectionsIcon,
+  computerscience: computerscienceIcon,
+  hat: hatIcon,
+  apollo: apollographql,
+};
